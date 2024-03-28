a2b4e24c7244652304b88653300f48a53bb2c5cb
admin <admin@adminui-MacBookPro.local>
2024-03-28 14:10:56 +0900

    [Fix] get cashe 삭제

--- routes/users.js
+++ routes/users.js
@@ -7,42 +7,42 @@ const router = express.Router();
 const cache = apicache.middleware;
 
 const updateApiStat = (req, res, next) => {
-  // update ApiStat
-  ApiStatCounter('users');
-  next();
+    // update ApiStat
+    ApiStatCounter('users');
+    next();
 };
 
 router.use(updateApiStat);
 
-router.get('/', cache('1 week'), async (req, res) => {
-  const users = await User.findAll();
-  res.json(users);
+router.get('/', async (req, res) => {
+    const users = await User.findAll();
+    res.json(users);
 });
 
 router.post('/', async (req, res) => {
-  /* name, username, email, city 필수값 */
-  const user = await User.create(req.body)
-  res.status(201).json(user);
+    /* name, username, email, city 필수값 */
+    const user = await User.create(req.body);
+    res.status(201).json(user);
 });
 
 router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const user = await User.findByPk(id);
-  res.status(200).json(user);
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    res.status(200).json(user);
 });
 
 router.put('/:id', async (req, res) => {
-  const { id } = req.params;
-  const user = await User.findByPk(id);
-  await user.update(req.body)
-  res.status(200).json(user);
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    await user.update(req.body);
+    res.status(200).json(user);
 });
 
 router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-  const user = await User.findByPk(id);
-  await User.destroy({where: {id:id}})
-  res.status(200).json(user);
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    await User.destroy({ where: { id: id } });
+    res.status(200).json(user);
 });
 
 module.exports = router;
